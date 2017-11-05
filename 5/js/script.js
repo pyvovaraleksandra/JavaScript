ed4b049f4ca826251c0087f64859c6f62e20e6b6
Aleksandra <PivovarAO@gmail.com>
2017-11-05 22:20:07 +0200

    add generateRange function

--- 5/js/script.js
+++ 5/js/script.js
@@ -17,3 +17,18 @@ var result = isSubString(str, subStr);
 console.log(result);
 
 
+// task 3
+function generateRange(min, max, step) {
+	if (min >= max || step <= 0)
+		return false;
+
+	var result = [];
+	for (var i = min; i <= max; i = i+step) {
+		result.push(i);
+	}
+
+	return result;
+}
+
+var arr = generateRange(1, 7, 1);
+console.log(arr);
