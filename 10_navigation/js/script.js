fc9fea23d3649565e5cf1de378dcc7584272b4f7
Aleksandra <PivovarAO@gmail.com>
2017-12-13 00:53:30 +0200

    Put functions to addEventListener. Optimize function toggleButton

--- 10_navigation/js/script.js
+++ 10_navigation/js/script.js
@@ -12,199 +12,204 @@ window.onload = function(){
 		var nextButton = document.getElementsByClassName("selector-next")[0];
 		allThemes.unshift(newThemeNo);
 	});
-};
 
-var prevButton, nextButton, parentButton, childButton, leftButton, rightButton;
-var elem = [];
-var current = 0;
-var vElem;
-var vertical = 0, sibl = 0;
+	// find
+	var	findButton = document.querySelector('.selector-find'),
+		nextButton = document.getElementsByClassName("selector-next")[0],
+		prevButton = document.getElementsByClassName("selector-prev")[0],
+		parentButton = document.getElementsByClassName("nav-top")[0],
+		childButton = document.getElementsByClassName("nav-bottom")[0],
+		leftButton = document.getElementsByClassName("nav-left")[0],
+		rightButton = document.getElementsByClassName("nav-right")[0];
 
-function findElement() {
-	resetAll();
-	current = 0;
+	var sibl = 0,
+		vertical = 0;
 
-	prevButton = document.getElementsByClassName("selector-prev")[0];
-	nextButton = document.getElementsByClassName("selector-next")[0];
-	parentButton = document.getElementsByClassName("nav-top")[0];
-	childButton = document.getElementsByClassName("nav-bottom")[0];
-	leftButton = document.getElementsByClassName("nav-left")[0];
-	rightButton = document.getElementsByClassName("nav-right")[0];
+	findButton.addEventListener('click', function() {
+		resetAll();
+		current = 0;
 
-	var input = document.getElementsByClassName("selector");
-	var selector = input[0].value.trim();
+		var input = document.getElementsByClassName("selector");
+		var selector = input[0].value.trim();
 
-	elem = document.querySelectorAll(selector);
+		if (!selector) {
+			alert("Please, write any selector");
+			return;
+		}
 
-	if(elem.length) {
-		color(elem[0]);
-	} else {
-		alert("There are not elements. Please, try another query");
-	}
-	
-	toggleButton(prevButton, false);
+		elem = document.querySelectorAll(selector);
 
-	if(elem[1])
-		toggleButton(nextButton, true);
-
-	if (elem[0].parentElement) 
-		toggleButton(parentButton, true);
-
-	if (elem[0].firstElementChild) 
-		toggleButton(childButton, true);
-
-	if (elem[0].previousElementSibling)
-		toggleButton(leftButton, true);
-
-	if (elem[0].nextElementSibling)
-		toggleButton(rightButton, true);
-}
-
-function findNext() {
-	current++;
-	color(elem[current]);
-	defaultColor(elem[current-1]);
-
-	if(elem[current-1])
-		toggleButton(prevButton, true);
-
-	if(!elem[current+1]) 
-		toggleButton(nextButton, false);
-
-	if (elem[current]) 
-		toggleButton(parentButton, true);
-
-	if (elem[current].previousElementSibling)
-		toggleButton(leftButton, true);
-
-	if (!elem[current].nextElementSibling)
-		toggleButton(rightButton, false);
-}
-
-function findPrev() {
-	current--;
-	color(elem[current]);
-	defaultColor(elem[current+1]);
-
-	toggleButton(nextButton, true);
-
-	if(!elem[current-1])
+		if (elem.length) {
+			color(elem[0]);
+		} else {
+			alert("There are not elements. Please, try another query");
+			return;
+		}
+		
 		toggleButton(prevButton, false);
 
-	if (elem[current]) 
-		toggleButton(parentButton, true);
+		if(elem[1])
+			toggleButton(nextButton, true);
 
-	if (elem[current].nextElementSibling)
-		toggleButton(rightButton, true);
+		if (elem[0].parentElement) 
+			toggleButton(parentButton, true);
 
-	if (!elem[current].previousElementSibling)
-		toggleButton(leftButton, false);
-}
+		if (elem[0].firstElementChild) 
+			toggleButton(childButton, true);
 
-// DOM tree
+		if (elem[0].previousElementSibling)
+			toggleButton(leftButton, true);
 
-function findParent() {
-	toggleButton(nextButton, false);
-	toggleButton(prevButton, false);
-	resetAll();
+		if (elem[0].nextElementSibling)
+			toggleButton(rightButton, true);
+	});
+	//next
+	nextButton.addEventListener('click', function() {
+		current++;
+		color(elem[current]);
+		defaultColor(elem[current-1]);
 
-	if (vertical == 0) {
-		vElem = elem[current].parentElement;
-		vertical--;
-	} else {
-		vElem = vElem.parentElement;
-	}
+		if(elem[current-1])
+			toggleButton(prevButton, true);
 
-	color(vElem);
+		if(!elem[current+1]) 
+			toggleButton(nextButton, false);
 
-	//check fot body!!!
-	if (!vElem.parentElement) 
-		toggleButton(parentButton, false);
+		if (elem[current]) 
+			toggleButton(parentButton, true);
 
-	if (vElem.firstChild) 
-		toggleButton(childButton, true);
+		if (elem[current].previousElementSibling)
+			toggleButton(leftButton, true);
 
-	if (!vElem.previousElementSibling)
-		toggleButton(leftButton, false);
+		if (!elem[current].nextElementSibling)
+			toggleButton(rightButton, false);
+		});
+	//previous
+	prevButton.addEventListener('click', function() {
+		current--;
+		color(elem[current]);
+		defaultColor(elem[current+1]);
 
-	if (!vElem.nextElementSibling) {
-		toggleButton(rightButton, false);
-	} else {
-		sibl = 1; //continue from here in findNextSibling(), not from elem[0]
-	}
-}
+		toggleButton(nextButton, true);
 
-function findChild() {
-	toggleButton(nextButton, false);
-	toggleButton(prevButton, false);
-	resetAll();
+		if(!elem[current-1])
+			toggleButton(prevButton, false);
 
-	if (vertical == 0) {
-		vElem = elem[current].firstElementChild;
-		vertical++;
-	} else {
-		vElem = vElem.firstElementChild;
-	}
+		if (elem[current]) 
+			toggleButton(parentButton, true);
 
-	if(vElem) {
+		if (elem[current].nextElementSibling)
+			toggleButton(rightButton, true);
+
+		if (!elem[current].previousElementSibling)
+			toggleButton(leftButton, false);
+	});
+	//----DOM TREE----
+	//parent
+	parentButton.addEventListener('click', function() {
+		toggleButton(nextButton, false);
+		toggleButton(prevButton, false);
+		resetAll();
+
+		if (vertical == 0) {
+			vElem = elem[current].parentElement;
+			vertical--;
+		} else {
+			vElem = vElem.parentElement;
+		}
+
 		color(vElem);
-	} else {
-		alert("There are not elements. Please, try another query");
-	}
-	
-	if (!vElem.firstElementChild) 
-		toggleButton(childButton, false);
 
-	if (!vElem.previousElementSibling)
-		toggleButton(leftButton, false);
+		//check fot body!!!
+		if (!vElem.parentElement) 
+			toggleButton(parentButton, false);
 
-	if (vElem.nextElementSibling) {
-		toggleButton(rightButton, true);
-		sibl = 1; //continue from here in findNextSibling(), not from elem[0]
-	}
-}
+		if (vElem.firstChild) 
+			toggleButton(childButton, true);
 
-function findPrevSibling() {
-	toggleButton(nextButton, false);
-	toggleButton(prevButton, false);
-	resetAll();
+		if (!vElem.previousElementSibling)
+			toggleButton(leftButton, false);
 
-	if (sibl == 0) {
-		vElem = elem[current].previousElementSibling;
-		sibl--;
-	} else {
-		vElem = vElem.previousElementSibling;
-	}
+		if (!vElem.nextElementSibling) {
+			toggleButton(rightButton, false);
+		} else {
+			sibl = 1; //continue from here in findNextSibling(), not from elem[0]
+		}
+	})
+	//first child
+	childButton.addEventListener('click', function() {
+		toggleButton(nextButton, false);
+		toggleButton(prevButton, false);
+		resetAll();
 
-	color(vElem);
+		if (vertical == 0) {
+			vElem = elem[current].firstElementChild;
+			vertical++;
+		} else {
+			vElem = vElem.firstElementChild;
+		}
 
-	if (!vElem.previousElementSibling)
-		toggleButton(leftButton, false);
+		if(vElem) {
+			color(vElem);
+		} else {
+			alert("There are not elements. Please, try another query");
+		}
+		
+		if (!vElem.firstElementChild) 
+			toggleButton(childButton, false);
 
-	if (vElem.nextElementSibling)
-		toggleButton(rightButton, true);
-}
+		if (!vElem.previousElementSibling)
+			toggleButton(leftButton, false);
 
-function findNextSibling() {
-	toggleButton(nextButton, false);
-	toggleButton(prevButton, false);
-	resetAll();
+		if (vElem.nextElementSibling) {
+			toggleButton(rightButton, true);
+			sibl = 1; //continue from here in findNextSibling(), not from elem[0]
+		}
+	});
+	//previous sibling
+	leftButton.addEventListener('click', function() {
+		toggleButton(nextButton, false);
+		toggleButton(prevButton, false);
+		resetAll();
 
-	if (sibl == 0) {
-		vElem = elem[current].nextElementSibling;
-		sibl++;
-	} else {
-		vElem = vElem.nextElementSibling;
-	}
+		if (sibl == 0) {
+			vElem = elem[current].previousElementSibling;
+			sibl--;
+		} else {
+			vElem = vElem.previousElementSibling;
+		}
 
-	color(vElem);
+		color(vElem);
 
-	if (!vElem.nextElementSibling)
-		toggleButton(rightButton, false);
+		if (!vElem.previousElementSibling)
+			toggleButton(leftButton, false);
 
-	if (vElem.previousElementSibling)
-		toggleButton(leftButton, true);
-}
+		if (vElem.nextElementSibling)
+			toggleButton(rightButton, true);
+	});
+	//next sibling
+	rightButton.addEventListener('click', function() {
+		toggleButton(nextButton, false);
+		toggleButton(prevButton, false);
+		resetAll();
+
+		if (sibl == 0) {
+			vElem = elem[current].nextElementSibling;
+			sibl++;
+		} else {
+			vElem = vElem.nextElementSibling;
+		}
+
+		color(vElem);
+
+		if (!vElem.nextElementSibling)
+			toggleButton(rightButton, false);
+
+		if (vElem.previousElementSibling)
+			toggleButton(leftButton, true);
+	});
+
+};
 
 function color(element) {
 	element.style.outline = "3px solid red";
@@ -226,9 +231,5 @@ function resetAll() {
 }
 
 function toggleButton(button, enable) {
-	if (enable) {
-		button.removeAttribute("disabled");
-	} else {
-		button.setAttribute("disabled", "disabled");
-	}
+	button.disabled = !enable;
 }
