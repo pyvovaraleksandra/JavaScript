d31a5e7254f914f36d4139d486b3711b091b49e2
Aleksandra <PivovarAO@gmail.com>
2018-01-11 02:34:19 +0200

    new version. doesn't work'

--- collaboration/public/js/scripts.js
+++ collaboration/public/js/scripts.js
@@ -4,33 +4,38 @@ var url = '/collaboration';
 var index, status;
 
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', url, false);
-xhr.send();
+table.addEventListener("click", function(event) {
 
-if (xhr.readyState == 4 && xhr.status != 200) {
-	alert( xhr.status + ': ' + xhr.statusText );
-} else {
-	console.log(xhr.responseText);
-	console.log("-");
+	for (var i = 0; i <= 99; i++) {
+		if (stars[i] == event.target) {
+			index = i;
+			status = stars[i].getAttribute("src");
+			status = (status == "star_off.gif" ) ? 1 : 0; 
+		}
+	}
+	params = "i=" + index + "&s=" + status;
 
-	// set pics for the all stars according to response
-	binStars = xhr.responseText;
-	for (var i = 0; i <= binStars.length-1; i++) {
-		stars[i].src = (binStars[i] == 0) ? "star_off.gif" : "star_on.gif";	
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", url + '?' + params, false);
+
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			console.log(xhr.responseText);
+			setStatus(xhr.responseText);
+		} else {
+			// console.log(xhr.readyState);
+		}
 	}
 
-	// handler. get index and status of the clicked star
-	table.addEventListener("click", function(event) {
-		for (var i = 0; i <= 99; i++) {
-			if(stars[i] == event.target) {
-				index = i;
-				status = stars[i].getAttribute("src");
-				status = (status == "star_off.gif" ) ? 1 : 0; 
-			}
-		}
-		
-		xhr.open("GET", url + "?i=" + index + "&s=" + status, false);
-		xhr.send();
-	}, false);
+    xhr.send();
+});
+
+
+function setStatus(binStars) {
+	// binStars = binStars.slice(1, -1);
+	// binStars = binStars.toString();
+	// console.log(binStars);
+	for (var i = 0; i <= binStars.length-1; i++) {
+			stars[i].src = (binStars[i] == 0) ? "star_off.gif" : "star_on.gif";	
+	}
 }
